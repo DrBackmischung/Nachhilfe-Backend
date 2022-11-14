fcb9755968ea82bd9d25a05cebc1c7bdd9239042
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-11-14 20:33:04 +0100

    initial

---
+++ src/main/java/de/wi2020sebgroup1/nachhilfe/NachhilfeApplication.java
@@ -0,0 +1,13 @@
+package de.wi2020sebgroup1.nachhilfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NachhilfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NachhilfeApplication.class, args);
+	}
+
+}
