a271025ab366578be0c604a5738f5d78c782430e
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-12-11 20:36:17 +0100

    transportationService

--- src/main/java/de/wi2020sebgroup1/nachhilfe/gateway/controller/DistanceController.java
+++ src/main/java/de/wi2020sebgroup1/nachhilfe/gateway/controller/DistanceController.java
@@ -1,5 +1,33 @@
 package de.wi2020sebgroup1.nachhilfe.gateway.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import de.wi2020sebgroup1.nachhilfe.gateway.Variables;
+
+@Controller
+@RestController
 public class DistanceController {
+	
+	@GetMapping("/distance/{start}/{end}")
+	public ResponseEntity<Object> get(@PathVariable String start, @PathVariable String end) {
+		
+		RestTemplate t = new RestTemplate();
+		String URL = Variables.transportationServiceURL+"/api/distance/"+start+"/"+end;
+		try {
+			ResponseEntity<Object> result = t.getForEntity(URL, Object.class);
+			return new ResponseEntity<Object>(result.getBody(), result.getStatusCode());
+		} catch(HttpClientErrorException e) {
+			e.printStackTrace();
+			return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+	}
 
 }
--- src/main/java/de/wi2020sebgroup1/nachhilfe/gateway/entities/Distance.java
+++
@@ -1,8 +0,0 @@
-package de.wi2020sebgroup1.nachhilfe.gateway.entities;
-
-import jakarta.persistence.Entity;
-
-@Entity
-public class Distance {
-
-}
