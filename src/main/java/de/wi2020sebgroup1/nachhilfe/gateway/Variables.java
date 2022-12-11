90a67e2fbdb8ad8388e762b6ae5cf957ebd5c47a
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-12-11 20:29:12 +0100

    variables ServiceURL

--- src/main/java/de/wi2020sebgroup1/nachhilfe/gateway/Variables.java
+++ src/main/java/de/wi2020sebgroup1/nachhilfe/gateway/Variables.java
@@ -4,6 +4,14 @@ public class Variables {
 
 	public static final String userServiceURL = "http://localhost:6001";
 	public static final String timetableServiceURL = "http://localhost:6002";
-	public static final String gamificationServiceURL = "http://localhost:6003";
+	public static final String gamificationServiceURL = "http://localhost:7000";
+	public static final String chatbotURL = "http://localhost:5002";
+	public static final String billingServiceURL = "http://localhost:5003";
+	public static final String paymentServiceURL = "http://localhost:5004";
+	public static final String messageServiceURL = "http://localhost:5005";
+	public static final String transportationServiceURL = "http://localhost:5006";
+	public static final String logServiceURL = "http://localhost:5007";
+	public static final String documentExtractionServiceURL = "http://localhost:5008";
+	public static final String chatbot = "http://localhost:8000";
 
 }
