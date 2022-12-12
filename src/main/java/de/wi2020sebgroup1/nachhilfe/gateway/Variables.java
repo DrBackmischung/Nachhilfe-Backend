f274e4cd5bcb26b5b42a8330e8bb9457d24891b3
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-12-12 15:19:53 +0100

    comments

--- src/main/java/de/wi2020sebgroup1/nachhilfe/gateway/Variables.java
+++ src/main/java/de/wi2020sebgroup1/nachhilfe/gateway/Variables.java
@@ -2,16 +2,16 @@ package de.wi2020sebgroup1.nachhilfe.gateway;
 
 public class Variables {
 
-	public static final String userServiceURL = "http://localhost:6001";
-	public static final String timetableServiceURL = "http://localhost:6002";
+	public static final String userServiceURL = "http://localhost:6001"; // fertig
+	public static final String timetableServiceURL = "http://localhost:6002"; // fertig
 	public static final String gamificationServiceURL = "http://localhost:7000";
 	public static final String chatbotURL = "http://localhost:5002";
-	public static final String billingServiceURL = "http://localhost:5003";
-	public static final String paymentServiceURL = "http://localhost:5004";
-	public static final String messageServiceURL = "http://localhost:5005";
-	public static final String transportationServiceURL = "http://localhost:5006";
-	public static final String logServiceURL = "http://localhost:5007";
+	public static final String billingServiceURL = "http://localhost:5003"; // fertig
+	public static final String paymentServiceURL = "http://localhost:5004"; // nicht verfolgt
+	public static final String messageServiceURL = "http://localhost:5005"; // nicht verfolgt
+	public static final String transportationServiceURL = "http://localhost:5006"; // fertig
+	public static final String logServiceURL = "http://localhost:5007"; // fertig
 	public static final String documentExtractionServiceURL = "http://localhost:5008";
-	public static final String accountingServiceURL = "http://localhost:8000";
+	public static final String accountingServiceURL = "http://localhost:8000"; // nicht relevant
 
 }
