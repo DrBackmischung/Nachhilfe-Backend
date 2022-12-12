c13538ea7c6512ea5ac370df380fbcf63a6913bd
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-12-12 16:42:43 +0100

    log in accountservice

--- src/main/java/de/wi2020sebgroup1/nachhilfe/gateway/controller/AccountController.java
+++ src/main/java/de/wi2020sebgroup1/nachhilfe/gateway/controller/AccountController.java
@@ -1,5 +1,6 @@
 package de.wi2020sebgroup1.nachhilfe.gateway.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -15,10 +16,15 @@ import org.springframework.web.client.RestTemplate;
 import de.wi2020sebgroup1.nachhilfe.gateway.Variables;
 import de.wi2020sebgroup1.nachhilfe.gateway.entities.Login;
 import de.wi2020sebgroup1.nachhilfe.gateway.entities.Registration;
+import de.wi2020sebgroup1.nachhilfe.gateway.service.LogService;
+import de.wi2020sebgroup1.nachhilfe.gateway.util.Log;
 
 @Controller
 @RestController
 public class AccountController {
+	
+	@Autowired
+	LogService logger;
 	
 	@GetMapping("/login")
 	public ResponseEntity<Object> login(@RequestBody Login l) {
@@ -27,15 +33,20 @@ public class AccountController {
 		t.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 		String URL = Variables.userServiceURL+"/login";
 		HttpEntity<Object> entity = new HttpEntity<Object>(l, new HttpHeaders());
+		logger.log(new Log("Login", "Start logging in", "Info", "UserService", null, null));
 		try {
 			ResponseEntity<Object> result = t.postForEntity(URL, entity, Object.class);
+			logger.log(new Log("Login", "Logged in", "Info", "UserService", null, null));
 			return new ResponseEntity<Object>(result.getBody(), result.getStatusCode());
 		} catch(HttpClientErrorException e) {
 			if(e.getMessage().contains("404")) {
+				logger.log(new Log("Login", "User not found", "Warning", "UserService", null, null));
 				return new ResponseEntity<Object>("User not found", HttpStatus.NOT_FOUND);
 			} else if(e.getMessage().contains("409")) {
+				logger.log(new Log("Login", "Wrong password", "Warning", "UserService", null, null));
 				return new ResponseEntity<Object>("Wrong password", HttpStatus.CONFLICT);
 			} else {
+				logger.log(new Log("Login", "Login error", "Warning", "UserService", null, null));
 				e.printStackTrace();
 				return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 			}
@@ -50,13 +61,17 @@ public class AccountController {
 		t.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 		String URL = Variables.userServiceURL+"/register";
 		HttpEntity<Object> entity = new HttpEntity<Object>(r, new HttpHeaders());
+		logger.log(new Log("Registration", "Start registration", "Info", "UserService", null, null));
 		try {
 			ResponseEntity<Object> result = t.postForEntity(URL, entity, Object.class);
+			logger.log(new Log("Registration", "Registration successfull", "Info", "UserService", null, null));
 			return new ResponseEntity<Object>(result.getBody(), result.getStatusCode());
 		} catch(HttpClientErrorException e) {
 			if(e.getMessage().contains("409")) {
+				logger.log(new Log("Registration", "Conflicting credentials", "Warning", "UserService", null, null));
 				return new ResponseEntity<Object>("Conflicting credentials", HttpStatus.CONFLICT);
 			} else {
+				logger.log(new Log("Registration", "Registration error", "Warning", "UserService", null, null));
 				e.printStackTrace();
 				return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 			}
