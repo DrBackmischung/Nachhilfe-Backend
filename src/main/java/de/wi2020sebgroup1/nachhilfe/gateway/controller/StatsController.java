34cdd389a7f378d21bdc7b61cce48d882580e321
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-12-12 16:33:35 +0100

    stats logger

--- src/main/java/de/wi2020sebgroup1/nachhilfe/gateway/controller/StatsController.java
+++ src/main/java/de/wi2020sebgroup1/nachhilfe/gateway/controller/StatsController.java
@@ -15,6 +15,7 @@ import org.springframework.web.client.HttpClientErrorException;
 
 import de.wi2020sebgroup1.nachhilfe.gateway.entities.Stats;
 import de.wi2020sebgroup1.nachhilfe.gateway.service.LogService;
+import de.wi2020sebgroup1.nachhilfe.gateway.util.Log;
 import graphql.kickstart.spring.webclient.boot.GraphQLRequest;
 import graphql.kickstart.spring.webclient.boot.GraphQLResponse;
 import graphql.kickstart.spring.webclient.boot.GraphQLWebClient;
@@ -33,10 +34,13 @@ public class StatsController {
 	@GetMapping("")
 	public ResponseEntity<Object> getAll() {
 		try {
+			logger.log(new Log("Query stats", "Query all stats", "Info", "GamificationService", null, null));
 			GraphQLRequest request = GraphQLRequest.builder().query("query { stats { id, userId, registerDate, learningPoints, teachingPoints, profilePoints } }").build();
 			GraphQLResponse response = graphQLWebClient.post(request).block();
+			logger.log(new Log("Query stats", "All stats queried", "Info", "GamificationService", null, null));
 			return new ResponseEntity<Object>(response.getFirstList(Object.class), HttpStatus.OK);
 		} catch(HttpClientErrorException e) {
+			logger.log(new Log("Query stats", "Error querying stats", "Warning", "GamificationService", null, null));
 			e.printStackTrace();
 			return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
@@ -46,10 +50,13 @@ public class StatsController {
 	@GetMapping("/id/{id}")
 	public ResponseEntity<Object> getById(@PathVariable String id) {
 		try {
+			logger.log(new Log("Query stats", "Query stats from a user", "Info", "GamificationService", null, null));
 			GraphQLRequest request = GraphQLRequest.builder().query("query { stat(id: \""+id+"\") { id, userId, registerDate, learningPoints, teachingPoints, profilePoints } }").build();
 			GraphQLResponse response = graphQLWebClient.post(request).block();
+			logger.log(new Log("Update stats", "Stat object was queried", "Info", "GamificationService", null, null));
 			return new ResponseEntity<Object>(response.getFirst(Object.class), HttpStatus.OK);
 		} catch(HttpClientErrorException e) {
+			logger.log(new Log("Query stats", "Error querying stats", "Warning", "GamificationService", null, null));
 			e.printStackTrace();
 			return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
@@ -59,10 +66,13 @@ public class StatsController {
 	@GetMapping("/user/{id}")
 	public ResponseEntity<Object> getByUser(@PathVariable String id) {
 		try {
+			logger.log(new Log("Query stats", "Query stats from a user", "Info", "GamificationService", null, null));
 			GraphQLRequest request = GraphQLRequest.builder().query("query { statByUser(userId: \""+id+"\") { id, userId, registerDate, learningPoints, teachingPoints, profilePoints } }").build();
 			GraphQLResponse response = graphQLWebClient.post(request).block();
+			logger.log(new Log("Update stats", "Stat object was queried", "Info", "GamificationService", null, null));
 			return new ResponseEntity<Object>(response.getFirst(Object.class), HttpStatus.OK);
 		} catch(HttpClientErrorException e) {
+			logger.log(new Log("Query stats", "Error querying stats", "Warning", "GamificationService", null, null));
 			e.printStackTrace();
 			return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
@@ -72,10 +82,13 @@ public class StatsController {
 	@GetMapping("/user/{id}/register")
 	public ResponseEntity<Object> getRegisterDate(@PathVariable String id) {
 		try {
+			logger.log(new Log("Query stats", "Query stats from a user", "Info", "GamificationService", null, null));
 			GraphQLRequest request = GraphQLRequest.builder().query("query { statByUser(userId: \""+id+"\") { id, userId, registerDate } }").build();
 			GraphQLResponse response = graphQLWebClient.post(request).block();
+			logger.log(new Log("Update stats", "Stat object was queried", "Info", "GamificationService", null, null));
 			return new ResponseEntity<Object>(response.getFirst(Object.class), HttpStatus.OK);
 		} catch(HttpClientErrorException e) {
+			logger.log(new Log("Query stats", "Error querying stats", "Warning", "GamificationService", null, null));
 			e.printStackTrace();
 			return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
@@ -85,10 +98,13 @@ public class StatsController {
 	@GetMapping("/user/{id}/lp")
 	public ResponseEntity<Object> getLP(@PathVariable String id) {
 		try {
+			logger.log(new Log("Query stats", "Query stats from a user", "Info", "GamificationService", null, null));
 			GraphQLRequest request = GraphQLRequest.builder().query("query { statByUser(userId: \""+id+"\") { id, userId, learningPoints } }").build();
 			GraphQLResponse response = graphQLWebClient.post(request).block();
+			logger.log(new Log("Update stats", "Stat object was queried", "Info", "GamificationService", null, null));
 			return new ResponseEntity<Object>(response.getFirst(Object.class), HttpStatus.OK);
 		} catch(HttpClientErrorException e) {
+			logger.log(new Log("Query stats", "Error querying stats", "Warning", "GamificationService", null, null));
 			e.printStackTrace();
 			return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
@@ -98,10 +114,13 @@ public class StatsController {
 	@GetMapping("/user/{id}/tp")
 	public ResponseEntity<Object> getTP(@PathVariable String id) {
 		try {
+			logger.log(new Log("Query stats", "Query stats from a user", "Info", "GamificationService", null, null));
 			GraphQLRequest request = GraphQLRequest.builder().query("query { statByUser(userId: \""+id+"\") { id, userId, teachingPoints } }").build();
 			GraphQLResponse response = graphQLWebClient.post(request).block();
+			logger.log(new Log("Update stats", "Stat object was queried", "Info", "GamificationService", null, null));
 			return new ResponseEntity<Object>(response.getFirst(Object.class), HttpStatus.OK);
 		} catch(HttpClientErrorException e) {
+			logger.log(new Log("Query stats", "Error querying stats", "Warning", "GamificationService", null, null));
 			e.printStackTrace();
 			return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
@@ -111,10 +130,13 @@ public class StatsController {
 	@GetMapping("/user/{id}/pp")
 	public ResponseEntity<Object> getPP(@PathVariable String id) {
 		try {
+			logger.log(new Log("Query stats", "Query stats from a user", "Info", "GamificationService", null, null));
 			GraphQLRequest request = GraphQLRequest.builder().query("query { statByUser(userId: \""+id+"\") { id, userId, profilePoints } }").build();
 			GraphQLResponse response = graphQLWebClient.post(request).block();
+			logger.log(new Log("Update stats", "Stat object was queried", "Info", "GamificationService", null, null));
 			return new ResponseEntity<Object>(response.getFirst(Object.class), HttpStatus.OK);
 		} catch(HttpClientErrorException e) {
+			logger.log(new Log("Query stats", "Error querying stats", "Warning", "GamificationService", null, null));
 			e.printStackTrace();
 			return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
@@ -124,10 +146,13 @@ public class StatsController {
 	@PostMapping("")
 	public ResponseEntity<Object> save(@RequestBody Stats u) {
 		try {
+			logger.log(new Log("Create stats", "Create stats for a user", "Info", "GamificationService", null, null));
 			GraphQLRequest request = GraphQLRequest.builder().query("mutation { add(userId: \""+u.getUserId()+"\", registerDate: \""+u.getRegistrationDate()+"\", learningPoints: "+u.getLearningPoints()+", teachingPoints: "+u.getTeachingPoints()+", profilePoints: "+u.getProfilePoints()+") { id, userId, registerDate, learningPoints, teachingPoints, profilePoints } }").build();
 			GraphQLResponse response = graphQLWebClient.post(request).block();
+			logger.log(new Log("Update stats", "Stat object was created", "Info", "GamificationService", null, null));
 			return new ResponseEntity<Object>(response.getFirst(Object.class), HttpStatus.OK);
 		} catch(HttpClientErrorException e) {
+			logger.log(new Log("Create stats", "Error creating stats", "Warning", "GamificationService", null, null));
 			e.printStackTrace();
 			return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
@@ -137,10 +162,13 @@ public class StatsController {
 	@PutMapping("/{id}")
 	public ResponseEntity<Object> update(@RequestBody Stats u, @PathVariable String id) {
 		try {
+			logger.log(new Log("Update stats", "Update stats for a user", "Info", "GamificationService", null, null));
 			GraphQLRequest request = GraphQLRequest.builder().query("mutation { update(id: \""+id+"\", learningPoints: "+u.getLearningPoints()+", teachingPoints: "+u.getTeachingPoints()+", profilePoints: "+u.getProfilePoints()+") { id, userId, registerDate, learningPoints, teachingPoints, profilePoints } }").build();
 			GraphQLResponse response = graphQLWebClient.post(request).block();
+			logger.log(new Log("Update stats", "Stat object was updated", "Info", "GamificationService", null, null));
 			return new ResponseEntity<Object>(response.getFirst(Object.class), HttpStatus.OK);
 		} catch(HttpClientErrorException e) {
+			logger.log(new Log("Update stats", "Error updating stats", "Warning", "GamificationService", null, null));
 			e.printStackTrace();
 			return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
