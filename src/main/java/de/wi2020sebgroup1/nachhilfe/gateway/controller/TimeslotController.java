f5ed8f304a9845f721fb484e0ae1cd801f37a8de
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-12-12 16:37:02 +0100

    log in timeslotservice

--- src/main/java/de/wi2020sebgroup1/nachhilfe/gateway/controller/TimeslotController.java
+++ src/main/java/de/wi2020sebgroup1/nachhilfe/gateway/controller/TimeslotController.java
@@ -1,5 +1,6 @@
 package de.wi2020sebgroup1.nachhilfe.gateway.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -20,21 +21,29 @@ import org.springframework.web.client.RestTemplate;
 
 import de.wi2020sebgroup1.nachhilfe.gateway.Variables;
 import de.wi2020sebgroup1.nachhilfe.gateway.entities.Timeslot;
+import de.wi2020sebgroup1.nachhilfe.gateway.service.LogService;
+import de.wi2020sebgroup1.nachhilfe.gateway.util.Log;
 
 @Controller
 @RestController
 @RequestMapping("/timeslots")
 public class TimeslotController {
 	
+	@Autowired
+	LogService logger;
+	
 	@GetMapping("")
 	public ResponseEntity<Object> getAll() {
 		
 		RestTemplate t = new RestTemplate();
 		String URL = Variables.timetableServiceURL+"/timeslots";
+		logger.log(new Log("Query timeslots", "All timeslots will be queried", "Info", "TimetableService", null, null));
 		try {
 			ResponseEntity<Object> result = t.getForEntity(URL, Object.class);
+			logger.log(new Log("Query timeslots", "All timeslots were queried", "Info", "TimetableService", null, null));
 			return new ResponseEntity<Object>(result.getBody(), result.getStatusCode());
 		} catch(HttpClientErrorException e) {
+			logger.log(new Log("Query timeslots", "Nothing was returned", "Warning", "TimetableService", null, null));
 			e.printStackTrace();
 			return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
@@ -46,13 +55,17 @@ public class TimeslotController {
 		
 		RestTemplate t = new RestTemplate();
 		String URL = Variables.timetableServiceURL+"/timeslots/"+id;
+		logger.log(new Log("Query timeslot", "One timeslot will be queried", "Info", "TimetableService", null, null));
 		try {
 			ResponseEntity<Object> result = t.getForEntity(URL, Object.class);
+			logger.log(new Log("Query timeslot", "One timeslot was queried", "Info", "TimetableService", null, null));
 			return new ResponseEntity<Object>(result.getBody(), result.getStatusCode());
 		} catch(HttpClientErrorException e) {
 			if(e.getMessage().contains("404")) {
+				logger.log(new Log("Query timeslot", "timeslot was not found", "Warning", "TimetableService", null, null));
 				return new ResponseEntity<Object>("Timeslot not found", HttpStatus.NOT_FOUND);
 			} else {
+				logger.log(new Log("Query timeslot", "Was not able to query timeslot", "Warning", "TimetableService", null, null));
 				e.printStackTrace();
 				return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 			}
@@ -67,13 +80,17 @@ public class TimeslotController {
 		t.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 		String URL = Variables.timetableServiceURL+"/timeslots";
 		HttpEntity<Object> entity = new HttpEntity<Object>(u, new HttpHeaders());
+		logger.log(new Log("Create timeslot", "timeslot will be created", "Info", "TimetableService", null, null));
 		try {
 			ResponseEntity<Object> result = t.postForEntity(URL, entity, Object.class);
+			logger.log(new Log("Create timeslot", "timeslot was created", "Info", "TimetableService", null, null));
 			return new ResponseEntity<Object>(result.getBody(), result.getStatusCode());
 		} catch(HttpClientErrorException e) {
 			if(e.getMessage().contains("409")) {
+				logger.log(new Log("Create timeslot", "timeslot already exist", "Warning", "TimetableService", null, null));
 				return new ResponseEntity<Object>("Conflicting credentials", HttpStatus.CONFLICT);
 			} else {
+				logger.log(new Log("Create timeslot", "timeslot was not created", "Warning", "TimetableService", null, null));
 				e.printStackTrace();
 				return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 			}
@@ -88,15 +105,20 @@ public class TimeslotController {
 		t.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 		String URL = Variables.timetableServiceURL+"/timeslots/"+id;
 		HttpEntity<Object> entity = new HttpEntity<Object>(u, new HttpHeaders());
+		logger.log(new Log("Update timeslot", "timeslot will be updated", "Info", "TimetableService", null, null));
 		try {
 			ResponseEntity<Object> result = t.exchange(URL, HttpMethod.PUT, entity, Object.class);
+			logger.log(new Log("Update timeslot", "timeslot was updated", "Info", "TimetableService", null, null));
 			return new ResponseEntity<Object>(result.getBody(), result.getStatusCode());
 		} catch(HttpClientErrorException e) {
 			if(e.getMessage().contains("404")) {
+				logger.log(new Log("Update timeslot", "timeslot was not found", "Warning", "TimetableService", null, null));
 				return new ResponseEntity<Object>("Timeslot not found", HttpStatus.NOT_FOUND);
 			} else if(e.getMessage().contains("409")) {
+				logger.log(new Log("Update timeslot", "Wrong credentials", "Warning", "TimetableService", null, null));
 				return new ResponseEntity<Object>("Wrong password", HttpStatus.CONFLICT);
 			} else {
+				logger.log(new Log("Update timeslot", "timeslot was not updated", "Warning", "TimetableService", null, null));
 				e.printStackTrace();
 				return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 			}
@@ -110,13 +132,17 @@ public class TimeslotController {
 		RestTemplate t = new RestTemplate();
 		String URL = Variables.timetableServiceURL+"/timeslots/"+id;
 		HttpEntity<Object> entity = new HttpEntity<Object>(null, new HttpHeaders());
+		logger.log(new Log("Delete timeslot", "timeslot will be deleted", "Info", "TimetableService", null, null));
 		try {
 			ResponseEntity<Object> result = t.exchange(URL, HttpMethod.DELETE, entity, Object.class);
+			logger.log(new Log("Delete timeslot", "timeslot was deleted", "Info", "TimetableService", null, null));
 			return new ResponseEntity<Object>(result.getBody(), result.getStatusCode());
 		} catch(HttpClientErrorException e) {
 			if(e.getMessage().contains("404")) {
+				logger.log(new Log("Delete timeslot", "timeslot was not found", "Warning", "TimetableService", null, null));
 				return new ResponseEntity<Object>("Timeslot not found", HttpStatus.NOT_FOUND);
 			} else {
+				logger.log(new Log("Delete timeslot", "timeslot was not deleted", "Warning", "TimetableService", null, null));
 				e.printStackTrace();
 				return new ResponseEntity<Object>("Server error: "+e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 			}
